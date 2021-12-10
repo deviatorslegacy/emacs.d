3fda0c332f379f7566529d5409332a6ff589dbac
Christopher Jarville <cjarville4@gmail.com>
2021-12-10 11:12:37 -0700

    Edited emacs dotfiles.
    The changes are unstable and further editing is required after
    this commit.

--- init.el
+++ init.el
@@ -1,6 +1,9 @@
 ;; Goodbye to hello
 (setq inhibit-startup-message t)
 
+;; Use clipboard
+(setq select-enable-clipboard t)
+
 (scroll-bar-mode -1) ; No Scrollbar
 (tool-bar-mode -1) ; No Toolbar
 (tooltip-mode -1) ; No Tooltips
@@ -9,17 +12,15 @@
 
 (setq visible-bell t)
 
-; Nerd Font!
+;; Nerd Font!
 (set-face-attribute 'default nil :font "Fira Code Nerd Font Mono" :height 140)
-(load-theme 'deeper-blue)
 
 ;; Initialize package sources
 
 (require 'package)
 
-(setq package-archives '(("melpa" . "https://melpa.org/packages/")
-			 ("org" . "https://orgmode.org/elpa")
-			 ("elpa" . "https://elpa.gnu.org/packages/")))
+(add-to-list 'package-archives '(("melpa" . "https://melpa.org/packages/")))
+
 (package-initialize)
 (unless package-archive-contents
   (package-refresh-contents))
@@ -31,15 +32,37 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
+(column-number-mode)
+(global-display-line-numbers-mode t)
+
+(dolist (mode '(org-mode-hook
+		term-mode-hook
+		eshell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
 (use-package command-log-mode)
 
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config
+  (setq which-key-idle-delay 0.3))
+
 (use-package swiper
-  :diminish
-  :ensure t)
+  :diminish)
 
 (use-package counsel
-  :diminish
-  :ensure t)
+  :bind (("M-x" . counsel-M-x)
+	 ("C-b" . counsel-ibuffer)
+	 ("C-x C-f" . counsel-find-file)
+	 :map minibuffer-local-map
+	 ("C-r" . counsel-minibuffer-history))
+  :config
+  (setq ivy-initial-inputs-alist nil)
+  :diminish)
 
 (use-package ivy
   :diminish
@@ -55,12 +78,86 @@
   :config
   (ivy-mode 1))
 
+(use-package ivy-rich
+  :init
+  (ivy-rich-mode 1))
+
+(use-package helpful
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . counsel-describe-function)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-key] . helpful-key))
+
 (use-package doom-modeline
-  :ensure t
-  :init (doom-modeline-mode 1))
+  :init (doom-modeline-mode 1)
+  :custom ((doom-modeline-height 40)))
 
 (use-package all-the-icons
   :if (display-graphic-p))
 
+(use-package doom-themes
+  :init
+  (load-theme 'doom-dark+ t)
+  :custom
+  (setq doom-themes-enable-bold t
+	doom-themes-enable-italic t)
+  (doom-themes-visual-bell-config)
+  (doom-themes-neotree-config)
+  (setq doom-themes-treemacs-theme "doom-atom")
+  (doom-themes-treemacs-config)
+  (doom-themes-org-config))
+
 ;; Buffer Magic
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+(global-set-key (kbd "C-;") 'counsel-switch-buffer)
+
+(defun cj/evil-hook ()
+  (dolist (mode '(custom-mode
+		  eshell-mode
+		  git-rebase-mode
+		  erc-mode
+		  circe-server-mode
+		  circe-chat-mode
+		  circe-query-mode
+		  sauron-mode
+		  term-mode))
+    (add-to-list 'evil-emacs-state-modes mode)))
+
+(use-package evil
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  (setq evil-want-C-u-scroll t)
+  (setq evil-want-C-i-jump t)
+  :hook (evil-mode . cj/evil-hook)
+  :config
+  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
+
+  ; Use visual motions outside of VISUAL line-mode buffers
+  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+  (evil-set-initial-state 'messages-buffer-mode 'normal)
+  (evil-set-initial-state 'dashboard-mode 'normal))
+
+(setq custom-file (concat user-emacs-directory "custom.el"))
+(load custom-file 'noerror)
+
+(evil-mode 1)
+
+;; C++ Configuration
+(require 'semantic)
+(global-semanticdb-minor-mode 1)
+(global-semantic-idle-scheduler-mode 1)
+(semantic-mode 1)
+(semantic-enable)
+
+;; Magit
+(use-package magit
+  :init
+  (progn
+    (bind-key "C-x g" 'magit-status)))
