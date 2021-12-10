ca2a5688babcfe2258aa1526346670621be72c85
Christopher Jarville <cjarville4@gmail.com>
2021-12-10 15:54:07 -0700

    C++ and Typescript support

--- custom.el
+++ custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(magit evil which-key use-package rainbow-delimiters ivy-rich helpful general doom-themes doom-modeline counsel command-log-mode)))
+   '(company tide modern-cpp-font-lock magit evil which-key use-package rainbow-delimiters ivy-rich helpful general doom-themes doom-modeline counsel command-log-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- init.el
+++ init.el
@@ -156,3 +156,30 @@
   :init
   (progn
     (bind-key "C-x g" 'magit-status)))
+
+;; C/C++
+(use-package modern-cpp-font-lock)
+
+;; Typescript
+(use-package tide)
+(use-package company)
+(use-package flycheck)
+
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1)
+  ;; company is an optional dependency. You have to
+  ;; install it separately via package-install
+  ;; `M-x package-install [ret] company`
+  (company-mode +1))
+
+;; aligns annotation to the right hand side
+(setq company-tooltip-align-annotations t)
+
+;; formats the buffer before saving
+(add-hook 'before-save-hook 'tide-format-before-save)
+(add-hook 'typescript-mode-hook #'setup-tide-mode)
